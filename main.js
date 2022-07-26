f2fce6619574a5f80c29ab54cd21dab906e672af
EnTeVi <andriy.lytvynchuk@gmail.com>
2022-07-27 00:24:18 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,98 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+let array = ["Iron man", 5>=4, "Mr.Dr.Strange", 100, "Capitan America", "Capitan Marvel", "Black Widow", "Tor", "Spiderman", Math.PI];
+
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
+console.log(array[5]);
+console.log(array[6]);
+console.log(array[7]);
+console.log(array[8]);
+console.log(array[9]);
+
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
+// let doctorSleep = {
+//     title: "Doctor Sleep",
+//     pageCount: 335,
+//     genre:  "horror, thriller"
+// };
+//
+//
+// let inferno = {
+//     title: "The Inferno",
+//     pageCount: 288,
+//     genre:  "adventures, thriller"
+// };
+//
+//
+// let lostSymbol = {
+//     title: "The Lost Symbol",
+//     pageCount: 315,
+//     genre:  "adventures, thriller"
+// };
+
+
+// - Створити 3 об'єкLти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+
+// let shining = {
+//     title: "The Shining",
+//     pageCount: 288,
+//     genre: "horror, thriller",
+//     authors: [
+//         {name: "Stephan King"},
+//         {age: 74}
+//     ]
+// }
+
+// let origin = {
+//     title: "Origin",
+//     pageCount: 256,
+//     genre: "adventures, thriller",
+//     authors: [
+//         {name: "Dan Browm"},
+//         {age: 58}
+//     ]
+// }
+//
+// let kobzar = {
+//     title:"Kobzar",
+//     pageCount: 115,
+//     genre: "poetry",
+//     authors: [
+//         {name: "Taras Shevchenko"},
+//         {age: 208}
+//     ]
+// }
+
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+// let user = [
+//     {name: "Tatas Shevchenko", username: "Sheva", password: "Yak-umru_to+poh"},
+//     {name: "Tatas Shevchenko", username: "Sheva", password: "Yak-umru_to+pohovay"},
+//     {name: "Tatas Shevchenko", username: "Sheva", password: "Yak-umru_to+pohovayte"},
+//     {name: "Tatas Shevchenko", username: "Sheva", password: "Yak-umru_to+pohovayte-mene"},
+//     {name: "Tatas Shevchenko", username: "Sheva", password: "Yak-umru_to+pohovayte-mene=na"},
+//     {name: "Tatas Shevchenko", username: "Sheva", password: "Yak-umru_to+pohovayte-mene=na$"},
+//     {name: "Tatas Shevchenko", username: "Sheva", password: "Yak-umru_to+pohovayte-mene=na#moguli"},
+//     {name: "Tatas Shevchenko", username: "Sheva", password: "Yak-umru_to+pohovayte-mene=na#moguli*sered"},
+//     {name: "Tatas Shevchenko", username: "Sheva", password: "Yak-umru_to+pohovayte-mene=na#moguli*sered%stepu"},
+//     {name: "Tatas Shevchenko", username: "Sheva", password: "Yak-umru_to+pohovayte-mene=na#moguli*sered%stepu#shurokogo!na@vkraini?muliy"}
+// ];
+//
+// console.log(user[0].password);
+// console.log(user[1].password);
+// console.log(user[2].password);
+// console.log(user[3].password);
+// console.log(user[4].password);
+// console.log(user[5].password);
+// console.log(user[6].password);
+// console.log(user[7].password);
+// console.log(user[8].password);
+// console.log(user[9].password);
